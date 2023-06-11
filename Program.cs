90cd9b6689b44c973e0a272beb0f4cdd86d55855
Natã Elienai <nata.elienai@gmail.com>
2023-06-10 21:09:38 -0300

    feat: configure cosmos db client and DI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
+using DesafioBackend.Repositories;
+using Microsoft.Azure.Cosmos;
+
+using CosmosClient client = new(
+    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+);
+
+Database database = await client.CreateDatabaseIfNotExistsAsync(
+    id: "DesafioBackend"
+);
+
+Container container = await database.CreateContainerIfNotExistsAsync(
+    id: "people",
+    partitionKeyPath: "/id",
+    throughput: 400
+);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +24,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<PersonRepository>(options => {
+    return new PersonRepository(container);
+});
 
 var app = builder.Build();
 
