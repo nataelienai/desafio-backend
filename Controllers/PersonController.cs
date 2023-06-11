071d072d96a98365e5ffc443c7bb2cce34e5f2f6
Natã Elienai <nata.elienai@gmail.com>
2023-06-10 21:08:37 -0300

    feat: create PersonController with HTTP POST action

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+using DesafioBackend.Models;
+using DesafioBackend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    private readonly PersonRepository _personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        _personRepository = personRepository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Person person)
+    {
+        var createdPerson = await _personRepository.Create(person);
+        return StatusCode(201, createdPerson);
+    }
+}
